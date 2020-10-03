8ff27bbecd510ddc5aa2a5cfba8af4812705c098
Lucky Ace Co <60290387+luckyaceco@users.noreply.github.com>
2020-10-03 07:30:07 +0100

    Begin merge with underscores

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package underpants
+ */
+
+?>
+
+
+
+				<footer id="colophon" class="site-footer" role="contentinfo">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underpants' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'underpants' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'underpants' ), 'underpants', '<a href="http://underscores.me/">Underscores.me</a>' );
+						?>
+				</footer>
+				</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<div id="page" class="site">
-    <header id="masthead" class="site-header" role="banner"><?php get_header(); ?></header>
-    <main id="main" class="site-main" role="main">
+<?php get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
       <?php if ( have_posts() ) :
               while ( have_posts() ) : the_post();
                   the_title();
@@ -10,7 +11,7 @@
           else :
               _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
             endif; ?>
-    </main>
-    <aside id="secondary" class="widget-area" role="complementary"><?php get_sidebar(); ?></aside>
-    <footer id="colophon" class="site-footer" role="contentinfo"><?php get_footer(); ?></footer>
-</div>
+</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
