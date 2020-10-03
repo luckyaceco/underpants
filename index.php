ca3b8d4205af41f57e7d1f7626d26ed7abd830d1
Lucky Ace Co <60290387+luckyaceco@users.noreply.github.com>
2020-10-03 02:13:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
-<?php
-get_header();
-
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-        the_content();
-    endwhile;
-else :
-    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-endif;
-
-get_sidebar();
-get_footer();
-?>
+<div id="page" class="site">
+    <header id="masthead" class="site-header" role="banner"><?php get_header(); ?></header>
+    <main id="main" class="site-main" role="main">
+      <?php if ( have_posts() ) :
+              while ( have_posts() ) : the_post();
+                  the_title();
+                  the_category(); the_tags(); the_author(); the_time();
+                  the_content();
+              endwhile;
+          else :
+              _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+            endif; ?>
+    </main>
+    <aside id="secondary" class="widget-area" role="complementary"><?php get_sidebar(); ?></aside>
+    <footer id="colophon" class="site-footer" role="contentinfo"><?php get_footer(); ?></footer>
+</div>
